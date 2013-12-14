ddd8d8bbb778528b583913585d9da194b20096c8
Weston Platter <westonplatter@gmail.com>
2013-12-13 22:26:55 -0700

    format ruby hashes

--- app/models/concerns/spree_mailchimp_gibbon/user_concern.rb
+++ app/models/concerns/spree_mailchimp_gibbon/user_concern.rb
@@ -36,8 +36,11 @@ module SpreeMailchimpGibbon
     def mailchimp_add_to_mailing_list
       if self.is_mail_list_subscriber?
         begin
-          gibbon.lists.subscribe( { id: mailchimp_list_id, email: { email: self.email }, merge_vars: mailchimp_merge_vars,
+          gibbon.lists.subscribe( { id: mailchimp_list_id, 
+                                    email: { email: self.email }, 
+                                    merge_vars: mailchimp_merge_vars,
                                     double_optin: false, send_welcome: true } )
+          
           logger.debug "Fetching new mailchimp subscriber info"
 
           assign_mailchimp_subscriber_id if self.mailchimp_subscriber_id.blank?
@@ -54,7 +57,11 @@ module SpreeMailchimpGibbon
       if !self.is_mail_list_subscriber? && self.mailchimp_subscriber_id.present?
         begin
           # TODO: Get rid of those magic values. Maybe add them as Spree::Config options?
-          gibbon.lists.unsubscribe(id: mailchimp_list_id, :email => { email: self.email }, delete_member: true, send_notify: true)
+          gibbon.lists.unsubscribe( id: mailchimp_list_id, 
+                                    email: { email: self.email }, 
+                                    delete_member: true, 
+                                    send_notify: true )
+          
           logger.debug "Removing mailchimp subscriber"
         rescue Exception => ex
           logger.warn "SpreeMailChimp: Failed to remove contact from Mailchimp: #{ex.message}\n#{ex.backtrace.join("\n")}"
@@ -80,7 +87,8 @@ module SpreeMailchimpGibbon
     # Returns the Mailchimp ID
     def assign_mailchimp_subscriber_id
       begin
-        response = gibbon.lists.member_info( { id: Spree::Config[:mailchimp_list_id], emails: [{ email: self.email }] })
+        response = gibbon.lists.member_info({ id: Spree::Config[:mailchimp_list_id], 
+                                              emails: [{ email: self.email }] })
 
         if response[:success] == 1
           member = response[:data][0]
